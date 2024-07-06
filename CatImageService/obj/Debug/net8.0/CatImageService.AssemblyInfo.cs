408c66249907c45ff218346265c16ff6f926c6e4
molsrg <molsrg.35@gmail.com>
2024-07-06 11:30:09 +0300

    fix: index.html

--- CatImageService/obj/Debug/net8.0/CatImageService.AssemblyInfo.cs
+++ CatImageService/obj/Debug/net8.0/CatImageService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatImageService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5e75713d8aff0d9d263f750bd9a0fbc6c4b031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a768d5fbb67b7e8fe8f512076935e2b86e00b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatImageService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatImageService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
